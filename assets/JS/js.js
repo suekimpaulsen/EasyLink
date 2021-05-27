b8d1f179c6054498dfee32a4ad85b0a196380433
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-05-27 16:03:14 -0400

    updated api fetch

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -1,36 +1,11 @@
-
-getLocation = () => {
-
-    navigator.geolocation.getCurrentPosition((position) => {
-        storeLoactionData(position)
-    });
-
-}
-
-//get coordinates of user's location
-storeLoactionData = (position) => {
-    latitude = position.coords.latitude
-
-    longitude = position.coords.longitude
-
-    console.log(latitude)
-    console.log(longitude);
-    fetchApiData();
-}
-
 fetchApiData = () => {
     
-    var restaurantApi = 'https://api.documenu.com/v2/restaurants/search/geo?lat=' + latitude + 
-    '&lon=' + longitude + 
-    '&distance=15&size=15&key=e3fb5dcdf4c00fbb833a184f0893222e'
+    var restaurantApi = 'https://api.documenu.com/v2/restaurants/search/geo?lat=36.16589&lon=-86.78444&distance=25&size=15&key=e3fb5dcdf4c00fbb833a184f0893222e'
 
 
     fetch(restaurantApi)
-    .then((response) => {
-        console.log(response);
-        
-        return response.json();      
-        
+    .then((response) => {        
+        return response.json();       
     })
     .then((response) => {
         console.log(response);
@@ -40,4 +15,4 @@ fetchApiData = () => {
 
 
 
-$('#get-started').click(getLocation);
+$('#get-started').click(fetchApiData);
