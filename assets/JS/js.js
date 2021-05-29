28b11e9b899a172ae75faa8023635479b3da51f9
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-29 17:08:25 -0500

    added some notes

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -13,3 +13,32 @@ fetchApiData = () => {
 }
 
 $('#get-started').click(fetchApiData);
+
+
+// when you click get started button
+// about you modal pops up
+
+
+
+// ACTIVITES
+// after fill out the form and hit save
+// activities page shows up
+// select the options that are given
+
+
+// REVIEWS
+// list out the options that were selected in the activities page
+
+
+// RESULTS
+// shows a map
+// recommend places to go
+
+
+
+// CONTACT US
+
+
+// NAVBAR-BURGER
+// directs to its page when clicked
+
