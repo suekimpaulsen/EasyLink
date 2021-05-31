07aead716820cecbcff0f6d3af32e0c6fe86b9ff
Nina Davis <nina.k.davis2@gmail.com>
2021-05-31 17:55:18 -0500

    new map API

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -44,7 +44,15 @@ fetchApiData = () => {
 clearScreen = () => {
 	$('.practice').remove();
 }
+// map of activiteies pulls up 
+let map;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
 // when you click get started button
 // about you modal pops up
 
