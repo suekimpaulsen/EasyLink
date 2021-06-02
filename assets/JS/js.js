c2e8b05c2daf15151f0236de4513108155e8cb2f
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-06-02 14:47:54 -0400

    update api search so it is based on user inputs

--- assets/JS/js.js
+++ assets/JS/js.js
@@ -1,20 +1,22 @@
-var currentInfo = {
-	options: ['restaurant', 'bar',]
-}
-
-
 getModalInputInfo = () => {
 	//store input values into vraiables
 	var name = $('input[type="text"]').val();
 	var email = $('input[type="email"]').val();
 	var age = $('input[id="age"]').val();
 	var cuisineType = $('input[id="cuisine-type"]').val();
+	var bars = $('#bar-options').val();
+	
+
 	//turn variables into objects
 	var currentInfo = {
 		name: name,
 		email: email,
 		age: age,
-		cuisineType: cuisineType
+		options: [cuisineType]
+	}
+
+	if (bars) {
+		currentInfo.options.push('bar');
 	}
 
 	saveAboutYou(currentInfo)
@@ -31,10 +33,8 @@ saveAboutYou = (currentInfo) => {
 	//save newest information to local storage
 	localStorage.setItem('about-you', JSON.stringify(aboutYouInfo));
 
-	// initMap(currentInfo);
+	initMap(aboutYouInfo);
 }
-
-
 
 clearScreen = () => {
 	$('.practice').remove();
@@ -44,40 +44,49 @@ clearScreen = () => {
 let map;
 let service;
 let infowindow;
-
 var request;
 var marker = [];
 
-initMap = () => {
+initMap = (aboutYouInfo) => {
 	map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 36.1627, lng: -86.7816 },
     zoom: 12,
   });
 
-  request = {
-	  location: { lat: 36.1627, lng: -86.7816 },
-	  radius: 5000,
-	  keyword: currentInfo.options,
-	  openNow: 'true',
-	  rankby: 'prominence'
-  }
+  	var aboutYouInfo = JSON.parse(localStorage.getItem('about-you'));
 
-  service = new google.maps.places.PlacesService(map);
+  	if(aboutYouInfo) {
+		for(i = 0; i < aboutYouInfo[0].options.length; i++) { 
+			searchForPlaces(aboutYouInfo[0].options[i]);
+		}
+	}
+}
 
-  service.nearbySearch(request, placeMarkersOnMap);
-
-  google.maps.event.addListener(map, 'rightclick', (e) => {
-	  map.setCenter(e.latLng);
-	  clearResults(markers);
-
-	  var request = {
-		  location: e.latLng,
-		  radius: 5000,
-		  keyword: currentInfo.options
-	  }
-	  service.nearbySearch(request, placeMarkersOnMap);
-  })
-  infowindow = new google.maps.InfoWindow();
+searchForPlaces = (currentOption) => {
+	request = {
+		location: { lat: 36.1627, lng: -86.7816 },
+		radius: 5000,
+		keyword: currentOption,
+		openNow: 'true',
+		rankby: 'prominence'
+	}
+  
+	service = new google.maps.places.PlacesService(map);
+  
+	service.nearbySearch(request, placeMarkersOnMap);
+  
+	google.maps.event.addListener(map, 'rightclick', (e) => {
+		map.setCenter(e.latLng);
+		clearResults(markers);
+  
+		var request = {
+			location: e.latLng,
+			radius: 5000,
+			keyword: currentOption
+		}
+		service.nearbySearch(request, placeMarkersOnMap);
+	})
+	infowindow = new google.maps.InfoWindow();
 }
 
 createCardsFromApi = (results) => {
@@ -88,37 +97,22 @@ createCardsFromApi = (results) => {
 	restaurantContainer.classList = 'practice';
 	$('#results').append(restaurantContainer);
 
-	for(i = 0; i < currentInfo.options.length; i++) {
-		
-		for(k = 0; k < results.length; k++) {
-
-			for(m = 0; m < results[k].types.length; m++) {
-
-				if(results[k].types[m] == currentInfo.options[i]) {
-					console.log(results[k].types[m]);
 					
-					var card = document.createElement('div');
-						$(card).addClass('card columns').appendTo('#results')
+	var card = document.createElement('div');
+		$(card).addClass('card columns').appendTo('#results')
 
-					var cardContent = document.createElement('div');
-						$(cardContent).addClass('card-content').appendTo(card)
+	var cardContent = document.createElement('div');
+		$(cardContent).addClass('card-content').appendTo(card)
 
-					var cardTitle = document.createElement('p')
-						$(cardTitle).addClass('title')
-						.html(results[k].name);
+	var cardTitle = document.createElement('p')
+		$(cardTitle).addClass('title')
+		.html(results[0].name);
 
-					var cardDetail = document.createElement('p')
-						$(cardDetail).addClass('subtitle')
-						.html(results[k].vicinity);
-						cardContent.append(cardTitle, cardDetail)
-					break;
-				}
-			}
-			break;	
-		}
-	}
+	var cardDetail = document.createElement('p')
+		$(cardDetail).addClass('subtitle')
+		.html(results[0].vicinity);
+		cardContent.append(cardTitle, cardDetail)
 
-	// getModalInputInfo();
 }
 
 placeMarkersOnMap = (results, status) => {
@@ -146,27 +140,6 @@ createMarker = (place) => {
 	});
 }
 
-// when you click get started button
-// about you modal pops up
-
-
-
-// ACTIVITES
-// after fill out the form and hit save
-// activities page shows up
-// select the options that are given
-
-
-// REVIEWS
-// list out the options that were selected in the activities page
-
-
-// RESULTS
-// shows a map
-// recommend places to go
-
-
-
 // CONTACT US
 var cEmail = $('input[type="text"]').val();
 var cExperience = $('input[type="option"]').val();
@@ -180,10 +153,6 @@ function contactUs() {
 		console.log ("this works")
 	})
 }
-// NAVBAR-BURGER
-// directs to its page when clicked
-
-
 
 
 class BulmaModal {
